15ce207fff2e205a3fe5cc3e67215140d06e0edf
Sergio <zerjioang@users.noreply.github.com>
2021-07-29 11:23:32 +0200

    Update env_env_test.go

--- env_env_test.go
+++ env_env_test.go
@@ -29,6 +29,25 @@ func TestEnv(t *testing.T) {
 	})
 }
 
+func TestIsTrue(t *testing.T) {
+	t.Run("case-yes", func(t *testing.T) {
+		assert.True(t, IsTrue("YES"))
+		assert.True(t, IsTrue("Yes"))
+		assert.True(t, IsTrue("yes"))
+		assert.True(t, IsTrue("on"))
+		assert.True(t, IsTrue("ACTIVE"))
+		assert.True(t, IsTrue("Active"))
+		assert.True(t, IsTrue("active"))
+		assert.True(t, IsTrue("TRUE"))
+		assert.True(t, IsTrue("True"))
+		assert.True(t, IsTrue("true"))
+		assert.True(t, IsTrue("1"))
+		assert.True(t, IsTrue("ENABLED"))
+		assert.True(t, IsTrue("Enabled"))
+		assert.True(t, IsTrue("enabled"))
+	})
+}
+
 func ExampleVariables() {
 	env := Variables()
 	val := env.GetInt64("MY_INT64_VALUE", 20)
